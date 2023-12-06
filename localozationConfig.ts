aee0a21ce8d2c5ccef45004f9ccff27882172884
Nama' Salameh <nama.salameh@gmail.com>
2023-12-07 01:44:43 +0200

    SS-9 Create string localization

---
+++ localozationConfig.ts
@@ -0,0 +1,8 @@
+import LocalizedStrings from "react-localization";
+import * as enTranslations from "./src/locals/en.json";
+
+const localization = new LocalizedStrings({
+  en: enTranslations,
+});
+
+export default localization;
