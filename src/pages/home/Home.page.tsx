c708f4192a9e3fff15e93112617e664f68e44e03
Nama' Salameh <nama.salameh@gmail.com>
2023-12-21 23:03:30 +0200

    SS-16 Create featured deals section functionality

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -3,8 +3,10 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import SearchBar from "../../components/bars/regularUser/searchBar/SearchBar.component";
 import localization from "../../localizationConfig";
+import { getFeaturedDealsHotels } from "../../services/home/home.service";
 
 export default function Home() {
+  const featuredDealsHotels = getFeaturedDealsHotels();
   return (
     <Box>
       <SearchBar />
