908ab6d86434100eb2cbb4e0b22828ba7e3afcd7
Nama-Salameh <nama.salameh@gmail.com>
2023-12-29 10:19:44 +0200

    SS-50 Search bar

---
+++ src/components/bars/admin/serachBar/SearchBar.component.tsx
@@ -0,0 +1,44 @@
+import React, { useState, ChangeEvent } from "react";
+import SmallButton from "../../../common/Buttons/SmallButton.component";
+import localization from "../../../../localizationConfig";
+import { TextField, InputAdornment } from "@mui/material";
+
+const SearchBar: React.FC<{ onSearch: (searchTerm: string) => void }> = ({
+  onSearch,
+}) => {
+  const [searchText, setSearchText] = useState<string>("");
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchText);
+  };
+
+  return (
+    <TextField
+      type="text"
+      placeholder="Search..."
+      variant="outlined"
+      value={searchText}
+      onChange={handleInputChange}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <SmallButton onClick={handleSearch} value={localization.search} />
+          </InputAdornment>
+        ),
+      }}
+      style={{
+        width: "100%",
+        margin: 0,
+        height: "52px",
+        borderRadius: "8px",
+        paddingRight: 0,
+      }}
+    />
+  );
+};
+
+export default SearchBar;
--- src/pages/admin/adminHotels/AdminHotels.page.tsx
+++ src/pages/admin/adminHotels/AdminHotels.page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import localization from "../../../localizationConfig";
+import { Box } from "@mui/material";
+import SearchBar from "../../../components/bars/admin/serachBar/SearchBar.component";
+
+const handleSearch = (searchText: string) => {};
 
 export default function AdminHotels() {
-  return <div>{localization.adminHotels}</div>;
+  return (
+    <Box component="main" sx={{ flexGrow: 1, p: 10, pt: 7, pr: 3 }}>
+      <SearchBar onSearch={handleSearch} />
+    </Box>
+  );
 }
