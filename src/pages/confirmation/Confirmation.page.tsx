9f65a923df926b1f53b103c4a74cd17e4dd77e6d
Nama-Salameh <nama.salameh@gmail.com>
2023-12-29 07:29:41 +0200

    SS-36 Create confirmation functionality and design

--- src/pages/confirmation/Confirmation.page.tsx
+++ src/pages/confirmation/Confirmation.page.tsx
@@ -8,6 +8,7 @@ import {
   faDollarSign,
   faClock,
   faFilePdf,
+  faFileDownload,
 } from "@fortawesome/free-solid-svg-icons";
 import style from "./Confirmation.module.css";
 import {
@@ -19,13 +20,17 @@ import {
   TableRow,
   Paper,
   Button,
+  IconButton,
 } from "@mui/material";
 import {
   handlePrintPdf,
   handleSavePdf,
 } from "../../utils/pdfGeneratorUtils/PdfGeneratoeUtils";
+import SmallButton from "../../components/common/Buttons/SmallButton.component";
+import { useTheme } from "@mui/system";
 
 export default function Confirmation() {
+  const theme = useTheme();
   const [bookingInfo, setBookingInfo] = useState({
     bookingDateTime: "",
     bookingStatus: "",
@@ -61,6 +66,44 @@ export default function Confirmation() {
   return (
     <div className={style.pageContainer} id="confirmationPage">
       <div className={style.bookingInfoContainer}>
+        <div className={style.fileeOptionsContainer}>
+          <IconButton
+            onClick={() => handlePrintPdf("confirmationPage")}
+            sx={{
+              backgroundColor: "var(--mui-palette-primary-main)",
+              fontSize: 20,
+              borderRadius: 5,
+              marginRight: 2,
+            }}
+            style={{
+              color: theme.palette.secondary.main,
+              backgroundColor: theme.palette.primary.main,
+              fontSize: 20,
+              borderRadius: 5,
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faFilePdf}
+              color="var(--mui-palette-secondary-main)"
+              fontSize="medium"
+            />
+          </IconButton>
+          <IconButton
+            onClick={() => handleSavePdf("confirmationPage")}
+            style={{
+              color: theme.palette.secondary.main,
+              backgroundColor: theme.palette.primary.main,
+              fontSize: 20,
+              borderRadius: 5,
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faFileDownload}
+              color="var(--mui-palette-secondary-main)"
+              fontSize="medium"
+            />
+          </IconButton>
+        </div>
         {bookingInfo.bookingStatus === "Confirmed" ? (
           <div>
             <div className={style.successConfirmContainer}>
@@ -70,50 +113,55 @@ export default function Confirmation() {
                 And please remember, you can booking another rooms at any time.
               </p>
             </div>
-            <TableContainer
-              component={Paper}
-              className={style.bookedRoomsTable}
-            >
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell className={style.boldTableCell}>
-                      Room Number
-                    </TableCell>
-                    <TableCell className={style.boldTableCell}>
-                      Room Type
-                    </TableCell>
-                    <TableCell className={style.boldTableCell}>
-                      Hotel Name
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow key={bookingInfo.roomNumber}>
-                    <TableCell>{bookingInfo.roomNumber}</TableCell>
-                    <TableCell>{bookingInfo.roomType}</TableCell>
-                    <TableCell>{bookingInfo.hotelName}</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <div className={style.paymentInfo}>
-              <p>
-                <FontAwesomeIcon icon={faCreditCard} className={style.icon} />
-                <b>Payment : </b> {bookingInfo.paymentMethod}
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faDollarSign} className={style.icon} />
-                <b>Total Cost : </b> {bookingInfo.totalCost} <b>$</b>
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faCalendarDays} className={style.icon} />
-                {formattedDate}
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faClock} className={style.icon} />
-                {formattedTime}
-              </p>
+            <div className={style.confirmationInfo}>
+              <TableContainer
+                component={Paper}
+                className={style.bookedRoomsTable}
+              >
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell className={style.boldTableCell}>
+                        Room Number
+                      </TableCell>
+                      <TableCell className={style.boldTableCell}>
+                        Room Type
+                      </TableCell>
+                      <TableCell className={style.boldTableCell}>
+                        Hotel Name
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow key={bookingInfo.roomNumber}>
+                      <TableCell>{bookingInfo.roomNumber}</TableCell>
+                      <TableCell>{bookingInfo.roomType}</TableCell>
+                      <TableCell>{bookingInfo.hotelName}</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <div className={style.paymentInfo}>
+                <p>
+                  <FontAwesomeIcon icon={faCreditCard} className={style.icon} />
+                  <b>Payment : </b> {bookingInfo.paymentMethod}
+                </p>
+                <p>
+                  <FontAwesomeIcon icon={faDollarSign} className={style.icon} />
+                  <b>Total Cost : </b> {bookingInfo.totalCost} <b>$</b>
+                </p>
+                <p>
+                  <FontAwesomeIcon
+                    icon={faCalendarDays}
+                    className={style.icon}
+                  />
+                  {formattedDate}
+                </p>
+                <p>
+                  <FontAwesomeIcon icon={faClock} className={style.icon} />
+                  {formattedTime}
+                </p>
+              </div>
             </div>
           </div>
         ) : (
@@ -124,12 +172,6 @@ export default function Confirmation() {
           </div>
         )}
       </div>
-      <Button onClick={() => handlePrintPdf("confirmationPage")}>
-        Print PDF
-      </Button>
-      <Button onClick={() => handleSavePdf("confirmationPage")}>
-        <FontAwesomeIcon icon={faFilePdf} />
-      </Button>
     </div>
   );
 }
